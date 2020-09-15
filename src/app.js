595f5807e1bccf3c5dc0ffc302a505ed19dcb1d3
Yun Chenxi <cleoxiii@163.com>
2020-09-15 16:28:28 +0800

    [yunchenxi] feat: should get basic user info and render on page.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 import "./style/index.scss";
+
+const rootURL = "http://localhost:8080";
+const { pathname } = window.location;
+const userWithIdURL = rootURL + pathname;
+const header = document.getElementsByClassName("header")[0];
+const avatar = document.getElementsByClassName("avatar")[0];
+
+function renderResumeHeader(data) {
+  avatar.src = `${data.avatar}`;
+  const userInfo = document.createElement("p");
+  userInfo.setAttribute("class", "user-info");
+  const textOfUserInfo = document.createTextNode(
+    `MY NAME IS ${data.name} ${data.age}YO AND THIS IS MY RESUME/CV`
+  );
+  userInfo.appendChild(textOfUserInfo);
+  header.appendChild(userInfo);
+}
+
+async function render() {
+  const response = await fetch(userWithIdURL);
+  const data = await response.json();
+  renderResumeHeader(data);
+}
+
+render();
